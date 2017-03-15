901ea4b754dfd07638eb54044e275eb33a49fbc4
김대원 <eodnjs2998@gmail.com>
2017-03-16 00:04:28 +0900

    add example

---
+++ example/Assets/Main.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    private void Awake()
+    {
+        Application.runInBackground = true;
+    }
+
+    private void OnEnable()
+    {
+        var instance = CLI.Bridge.TryInstall(1234, welcomeMessage: "hello");
+        instance.ExecuteCmd = ExecuteCmd;
+    }
+
+    private void OnDestroy()
+    {
+        CLI.Bridge.Uninstall();
+    }
+
+    private CLI.Result ExecuteCmd(CLI.Command cmd)
+    {
+        return CLI.Result.Success(cmd.Raw);
+    }
+}
